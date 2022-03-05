3ddd2abec6298d4acfcee49cc844fd6b9b52a500
Felicia <felicia@mage.ai>
2022-03-04 19:20:56 -0800

    populate data by row instead of column

--- fetch_api_test.py
+++ fetch_api_test.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 import json
 import os
 
-# fetches 1 game from 1 summoner as a test
-
 load_dotenv()
 
 API_URL = 'na1.api.riotgames.com/lol/'
@@ -15,13 +13,15 @@ MATCH_ID_ENDPOINT = 'match/v5/matches/by-puuid/'
 MATCH_INFORMATION_ENDPOINT = 'match/v5/matches/'
 SUMMONER_BY_NAME = 'summoner/v4/summoners/by-name/'
 
+# fetches 1 game from 1 summoner as a test
 peter = 'meth%20damon'
+wonton = 'NotWontonSoup'
 
 # returns puuid (str)
 def get_puuid(summoner_name):
 	get_url = 'https://' + API_URL + SUMMONER_BY_NAME
 	r = requests.get(get_url + summoner_name, 
-									headers={"X-Riot-Token": API_KEY})
+				headers={"X-Riot-Token": API_KEY})
 
 	# response
 	data = r.json()
@@ -32,7 +32,7 @@ get 20 match ids for each summoner
 	params:
 		puuid: uniquely identifies summoner
 	returns:
-		a list of 20 ids of the last 20 games played
+		a list of ids of the last 20 ranks games played
 """
 def get_matches(puuid):
 
@@ -86,7 +86,7 @@ def get_match_info(match_id):
 
 	picks = []
 	for players in game_data["info"]["participants"]:
-		picks.append(players["championId"])
+		picks.append(players["championName"])
 	print(picks)
 
 	kills = []
@@ -104,7 +104,7 @@ def get_match_info(match_id):
 		assists.append(players["assists"])
 	print(assists)
 
-
+	
 	stats = {}
 	# what are we returning?
 	stats = {
@@ -115,28 +115,100 @@ def get_match_info(match_id):
 		"deaths": deaths,
 		"assists": assists
 	}
-	s = json.dumps(stats, indent = 4) 
-	f = open("peter_data", "w+")
-	f.write(s)
-	f.close()
 
-
+	
 	"""
-	for team in game_data["info"]["teams"]:
-		print(team)
-
+	with open('wontons_data.json', 'w') as s:
+		s.write(json.dumps(stats, indent = 4))
 	"""
-
 	"""
-	info = str(r.json()["info"])
-	# print(game_data["info"]["participants"][0]["baronKills"]) # make some of stuff columns
-	f = open("peter_data", "w+")
-	f.write(info)
-	f.close()
+	skillshots = []
+	for players in game_data["info"]["participants"]:
+		skillshots.append(players["challenges"]["skillshotsDodged"])
+
+	print(skillshots)
 	"""
 
-get_match_info('NA1_4226412134')
+# peter's match info
+# get_match_info('NA1_4226412134')
+# wonton
+# get_match_info('NA1_4236804806')
 
+
+def test(match_id):
+	get_url = 'https://' + REGION_URL + MATCH_INFORMATION_ENDPOINT
+	
+	r = requests.get(get_url + match_id,
+	 headers={"X-Riot-Token": API_KEY})
+
+	game_data = r.json()
+
+	rows = []
+
+	
+
+	"""picks = []
+	for players in game_data["info"]["participants"]:
+		picks.append(players["championName"])
+	print(picks)
+
+	bans = []
+	for team in game_data["info"]["teams"]: 
+		for ban in team["bans"]:
+			bans.append(ban["championId"])
+	print(bans)
+
+	for players in game_data["info"]["participants"]:
+		winners.append(players["win"])
+	print(winners)
+	
+	kills = []
+	for players in game_data["info"]["participants"]:
+		kills.append(players["kills"])
+	print(kills)
+
+	deaths = []
+	for players in game_data["info"]["participants"]:
+		deaths.append(players["deaths"])
+	print(deaths)
+
+	assists = []
+	for players in game_data["info"]["participants"]:
+		assists.append(players["assists"])
+	print(assists)
+	"""
+
+	index = 0
+	# populate each row with a player's data
+	for player in game_data["info"]["participants"]:
+		row = {}
+		# Ids
+		row["matches"] = match_id
+		row["picks"] = player["championName"]
+		print("picks: ", player["championName"])
+
+		# there are 2 teams
+		if index < 5:
+			row["bans"] = game_data["info"]["teams"][0]["bans"][index]["championId"]
+		else:
+			# mod index so index of 2nd team starts at 0 too
+			row["bans"] = game_data["info"]["teams"][1]["bans"][index%5]["championId"]
+
+		row["kills"] = player["kills"]
+		row["deaths"] = player["deaths"]
+		row["assists"] = player["assists"]
+		row["win"] = player["win"]
+		rows.append(row)
+		index += 1
+	
+	# print(row)
+	# print (game_data["info"]["teams"])
+	# print(game_data["info"]["teams"][0]["bans"][0]["championId"])
+	with open('peter_data_2.json', 'w') as s:
+		s.write(json.dumps(rows, indent = 4))
+	print(rows)
+
+test('NA1_4226412134')
 # test
 # print(get_puuid(peter))
 # '-oXMqiG7Iz4jfAcbhR09AeP44KvBCtL9cEejVh-adG5LlQ0PEQFLJSwJV0Xk7upjLNKm5l3fygLhWA'
